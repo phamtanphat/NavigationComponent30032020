3fdec1aae8d269ae5583a8f0c138b565dc6e78d2
phatdroid94 <phatdroid94@gmail.com>
2020-07-15 20:55:13 +0700

    navigate

--- app/src/main/java/com/example/navigationcomponent30032020/LoginFragment.java
+++ app/src/main/java/com/example/navigationcomponent30032020/LoginFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -33,7 +35,9 @@ public class LoginFragment extends Fragment {
         mSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Navigation
+                        .findNavController(mView)
+                        .navigate(R.id.action_loginFragment_to_homeFragment);
             }
         });
         super.onActivityCreated(savedInstanceState);
--- app/src/main/java/com/example/navigationcomponent30032020/MainActivity.java
+++ app/src/main/java/com/example/navigationcomponent30032020/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.navigationcomponent30032020;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Fragment mHost;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mHost = getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+
+        NavHostFragment
+                .findNavController(mHost)
+                .navigate(R.id.action_loginFragment_to_homeFragment);
     }
 }
